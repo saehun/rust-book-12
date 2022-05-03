f72e9c7b2e374527b9c03b2943a1947239067f7a
minidonut <nycom13@gmail.com>
2022-05-04 02:04:59 +0900

    04

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,10 @@
-use std::{env, fs};
+use std::env;
+use std::fs;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
 
-    let query = &args[1];
-    let filename = &args[2];
+    let (query, filename) = parse_config(&args);
 
     println!("Searching for {}", query);
     println!("In file {}", filename);
@@ -12,4 +12,11 @@ fn main() {
     let contents = fs::read_to_string(filename).expect("Something went wrong reading the file");
 
     println!("With text:\n{}", contents);
+}
+
+fn parse_config(args: &[String]) -> (&str, &str) {
+    let query = &args[1];
+    let filename = &args[2];
+
+    (query, filename)
 }
