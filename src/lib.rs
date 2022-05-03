9572683943866eedd586d7f48e8625af692dc886
minidonut <nycom13@gmail.com>
2022-05-04 02:37:41 +0900

    18

--- src/lib.rs
+++ src/lib.rs
@@ -41,6 +41,19 @@ pub fn search<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
     results
 }
 
+pub fn search_case_insensitive<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
+    let query = query.to_lowercase();
+    let mut results = Vec::new();
+
+    for line in contents.lines() {
+        if line.to_lowercase().contains(&query) {
+            results.push(line);
+        }
+    }
+
+    results
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
