84e8d264e2d0c66ee88245eee4856890e22aa552
minidonut <nycom13@gmail.com>
2022-05-04 01:54:49 +0900

    03

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-use std::env;
+use std::{env, fs};
 
 fn main() {
     let args: Vec<String> = env::args().collect();
@@ -8,4 +8,8 @@ fn main() {
 
     println!("Searching for {}", query);
     println!("In file {}", filename);
+
+    let contents = fs::read_to_string(filename).expect("Something went wrong reading the file");
+
+    println!("With text:\n{}", contents);
 }
